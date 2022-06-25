c137ec417d6f1cfd1f00911d51f2559e9a826413
tavs <82376053+7iquid@users.noreply.github.com>
2022-06-25 20:41:26 +0800

    staticfiles dirs  errors when deplying to production

--- MainApp/settings.py
+++ MainApp/settings.py
@@ -140,10 +140,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
+
+if DEBUG:
+    STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'frontend-react-ts/build/static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
