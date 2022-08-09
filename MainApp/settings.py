861b14e2da515755890c92d137da818d21ba8449
tavs <82376053+7iquid@users.noreply.github.com>
2022-08-09 14:18:40 +0800

    uypdate

--- MainApp/settings.py
+++ MainApp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-4%5=hdooeuuta-ftcekk=uar^bxm%x2yvg&69#4il4_92_u&(z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ktc-ph-ui.herokuapp.com']
+ALLOWED_HOSTS = ['ktc-ph-ui.herokuapp.com','127.0.0.1' ]
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_RENDERER_CLASSES': (
---
+++ backendapi/DropBoxKo/DropBoxKo.py
@@ -0,0 +1,6 @@
+import dropbox
+
+App_key = 'w5lbt5pc42jrm20'
+App_secret = 'bhdxtu2vm0sqrgn'
+access_token = 'sl.BM9Cw0Cfmb7tac4Ml-nM2YTK1aUKGAujUEimOmqXUws-7fUuDnwy6QrGsJ1ystNeE12saWsbd6dTiesvHMyVLaaJ51RfEFMSS0ULOkMJv3SUvhG5VXfDL9CnZXCLfiCx6nuxyZs'
+
