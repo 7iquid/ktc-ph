ee8187a4995a04212dd0967e1bdbe7a351cfbe70
tavs <82376053+7iquid@users.noreply.github.com>
2022-08-19 11:25:04 +0800

    modify model

--- Accounts/models.py
+++ Accounts/models.py
@@ -10,10 +10,10 @@ from django.contrib.auth.models import User
 # TOKEN = settings.DROPBOX_OAUTH2_TOKEN
 # dbx = dropbox.Dropbox(settings.DROPBOX_OAUTH2_TOKEN)
 class UserThumbNail(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     # say = models.CharField(null = True, max_length=250 )
-    # date = models.DateTimeField(auto_now_add=True)
-    photo = models.ImageField(upload_to='photo')
+    date = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='photo' , null = True,)
     # image = models.FileField()
     # image_data = models.BinaryField(null=True)
     # player_photo = models.ImageField(upload_to="player_photos", storage=DatabaseStorage() )
