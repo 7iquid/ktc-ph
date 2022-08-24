fbac20d514c38636b3a20cbaeb9ec9e3ad8cae53
tavs <82376053+7iquid@users.noreply.github.com>
2022-08-24 23:07:51 +0800

    mod

--- backendapi/models.py
+++ backendapi/models.py
@@ -7,8 +7,8 @@ class NewsFeeds(models.Model):
 	_id = models.AutoField(null=False, primary_key= True)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	date = models.DateTimeField(auto_now_add=True)
-	category = models.CharField(max_length=200, null=False )
-	photo = models.ImageField(null=False, upload_to='newsfeeds')
+	category = models.CharField(max_length=200, null=False,default=None )
+	photo = models.ImageField(null=False, upload_to='newsfeeds', default=None)
 
 	def __str__(self):
 		return self.user
