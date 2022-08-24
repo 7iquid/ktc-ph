edcfabd9172e30aad894be64a7871e4f0be5f2dd
tavs <82376053+7iquid@users.noreply.github.com>
2022-08-24 23:59:29 +0800

    mod

--- backendapi/urls.py
+++ backendapi/urls.py
@@ -1,15 +1,15 @@
-from django.urls import path, include
-from .views import  NewsFeedsView ,NewsFeedsViewSet
-from rest_framework import routers
+from django.urls import path
+from .views import  NewsFeedsView 
+# from rest_framework import routers
 
 
-router = routers.DefaultRouter()
-router.register('NewsFeeds', viewset=NewsFeedsViewSet)
+# router = routers.DefaultRouter()
+# router.register('NewsFeeds', viewset=NewsFeedsViewSet)
 
 urlpatterns = [
 	path('', NewsFeedsView),
-	path('router', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+	# path('router', include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
 ]
 
--- backendapi/views.py
+++ backendapi/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth.models import User
 
 
 
-@api_view(['GET','POST'])
+@api_view(['GET'])
 def NewsFeedsView(request):
 	item = NewsFeeds.objects.all()
 	serializer = NewsFeedsSerializer(item, many=True)
@@ -22,32 +22,13 @@ def NewsFeedsView(request):
 
 
 
-class NewsFeedsViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    A simple ViewSet for viewing accounts.
-    """
-    serializer_class = NewsFeedsSerializer
-    queryset = NewsFeeds.objects.all()
+# class NewsFeedsViewSet(viewsets.ReadOnlyModelViewSet):
+#     """
+#     A simple ViewSet for viewing accounts.
+#     """
+#     serializer_class = NewsFeedsSerializer
+#     queryset = NewsFeeds.objects.all()
    
 
 
 
-# class NewsFeedsViewSet(viewsets.ModelViewSet):
-#     serializer_class = NewsFeedsSerializer
-#     queryset = NewsFeeds.objects.all()    
-
-
-# class UsersViewSet(viewsets.ViewSet):
-#     """
-#     A simple ViewSet for listing or retrieving users.
-#     """
-#     def list(self, request):
-#         queryset = User.objects.all()
-#         serializer = UsersSerializer(queryset, many=True)
-#         return Response(serializer.data)
-
-#     def retrieve(self, request, pk=None):
-#         queryset = User.objects.all()
-#         user = get_object_or_404(queryset, pk=pk)
-#         serializer = UsersSerializer(user)
-#         return Response(serializer.data)
